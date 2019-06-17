5db9100fba6835f6d67d0f34e41f6adb128f5237
Hzimr <51082717+Hzimr@users.noreply.github.com>
2019-06-16 22:46:28 -0300

    Add files via upload

---
+++ AppBDTur/src/Pesquisa/Museu.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pesquisa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author hital
+ */
+public class Museu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final String DRIVER = "com.mysql.jdbc.Driver";
+        final String DATABASE_URL = "jdbc:mysql://localhost:3307/bdtur";
+        final String SELECT_QUERY ="SELECT * FROM Museu as m inner join Ponto_Turistico as pt on m.PT_ID = pt.ID_PT  " ;
+       
+/*Hotel as h inner join Casa_de_Show as cs on h.Restaurante_ID = cs.Restaurante_ID */
+
+// usa try com recursos para conectar-se e consultar o banco de dados
+        try ( 
+        Connection connection = DriverManager.getConnection( 
+            DATABASE_URL, "root", "root1234"); 
+        Statement statement = connection.createStatement(); 
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY)) 
+        {
+// obtém os metadados de ResultSet
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int numberOfColumns = metaData.getColumnCount(); 
+
+        System.out.printf("Tabela de Museus:%n%n");
+// exibe os nomes de coluna no ResultSet
+        for (int i = 1; i <= numberOfColumns; i++)
+            System.out.printf("%-8s\t", metaData.getColumnName(i));
+        System.out.println();
+// exibe os resultados da consulta
+        while (resultSet.next())
+        {
+            for (int i = 1; i <= numberOfColumns; i++)
+                System.out.printf("%-8s\t", resultSet.getObject(i));
+            System.out.println();
+        }
+       } // Os métodos close dos objetos AutoCloseable são chamados agoraÿ
+        catch (SQLException sqlException)
+        {
+        sqlException.printStackTrace();
+        }
+    }
+} // fim da classe PontoTuristicoDescricao
+
+
